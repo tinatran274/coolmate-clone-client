af0f859e0bf3be2a5792b6814cc946a24616a209
Groolt 98 <binhnguyen9803@gmail.com>
2023-12-03 15:44:19 +0700

    Chart Dashboard

--- app/(pages)/admin/(dashboard)/Lifetimesales.jsx
+++ app/(pages)/admin/(dashboard)/Lifetimesales.jsx
@@ -1,16 +1,21 @@
 /* eslint-disable camelcase */
 'use client'
 import React from 'react'
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts'
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts'
 
 import './Lifetimesales.scss'
 import { Card } from './Card'
 import Dot from './Dot'
 
 const COLORS = ['#aee9d1', '#fed3d1', '#a4e8f2']
-
+const RADIAN = Math.PI / 180
 export default function LifetimeSale({ api }) {
-  const [data, setData] = React.useState({})
+  const data = {
+    orders: 3634,
+    total: '$102,420,193.60',
+    completed_percentage: 57,
+    cancelled_percentage: 32
+  }
   const [fetching, setFetching] = React.useState(true)
   const { orders, total, completed_percentage, cancelled_percentage } = data
 
@@ -22,6 +27,35 @@ export default function LifetimeSale({ api }) {
       value: 100 - completed_percentage - cancelled_percentage
     }
   ]
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index,
+    ...rests
+  }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5
+    const x = cx + radius * Math.cos(-midAngle * RADIAN)
+    const y = cy + radius * Math.sin(-midAngle * RADIAN)
+    console.log(
+      '{ cx, cy, midAngle, innerRadius, outerRadius, percent, index }',
+      { cx, cy, midAngle, innerRadius, outerRadius, percent, index, rests }
+    )
+    return (
+      <text
+        x={x}
+        y={y}
+        fill="white"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
+    )
+  }
 
   //   if (window !== undefined) {
   //     fetch(api, {
@@ -61,56 +95,64 @@ export default function LifetimeSale({ api }) {
   //   );
   // } else {
   return (
-    <Card title="Lifetime Sales">
-      <Card.Session>
-        <div className="grid grid-cols-1 gap-1">
-          <div className="flex space-x-2 items-center">
-            <Dot color="#a4e8f2" />
-            <div className="self-center">{orders} orders</div>
-          </div>
-          <div className="flex space-x-2 items-center">
-            <Dot color="#a4e8f2" />
-            <div className="self-center">{total} lifetime sale</div>
-          </div>
-          <div className="flex space-x-2 items-center">
-            <Dot color="#aee9d1" />
-            <div className="self-center">
-              {completed_percentage}% of orders completed
+    <div className="w-full h-full space-y-4">
+      <Card title="Lifetime Sales">
+        <Card.Session>
+          <div className="grid grid-cols-1 gap-1">
+            <div className="flex space-x-2 items-center">
+              <Dot color="#a4e8f2" />
+              <div className="self-center text-sm">{orders} orders</div>
+            </div>
+            <div className="flex space-x-2 items-center">
+              <Dot color="#a4e8f2" />
+              <div className="self-center text-sm">{total} lifetime sale</div>
+            </div>
+            <div className="flex space-x-2 items-center">
+              <Dot color="#aee9d1" />
+              <div className="self-center text-sm">
+                {completed_percentage}% of orders completed
+              </div>
+            </div>
+            <div className="flex space-x-2 items-center">
+              <Dot color="#fed3d1" />
+              <div className="self-center text-sm">
+                {cancelled_percentage}% of orders cancelled
+              </div>
             </div>
           </div>
-          <div className="flex space-x-2 items-center">
-            <Dot color="#fed3d1" />
-            <div className="self-center">
-              {cancelled_percentage}% of orders cancelled
-            </div>
+        </Card.Session>
+      </Card>
+      <Card>
+        <Card.Session>
+          <div style={{ height: '200px' }}>
+            <ResponsiveContainer width="100%" height="100%">
+              <PieChart>
+                <Pie
+                  data={chartData}
+                  cx="50%"
+                  cy="50%"
+                  labelLine={false}
+                  label={renderCustomizedLabel}
+                  outerRadius={80}
+                  fill="#8884d8"
+                  dataKey="value"
+                >
+                  {
+                    // eslint-disable-next-line react/no-array-index-key
+                    chartData.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))
+                  }
+                </Pie>
+                <Tooltip labelStyle={{ fontSize: 14 }} />
+              </PieChart>
+            </ResponsiveContainer>
           </div>
-        </div>
-      </Card.Session>
-      <Card.Session>
-        {/* <div style={{ height: '200px' }}>
-          <ResponsiveContainer width="100%" height="100%">
-            <PieChart>
-              <Pie
-                data={chartData}
-                labelLine={false}
-                fill="#8884d8"
-                dataKey="value"
-                label
-              >
-                {
-                  // eslint-disable-next-line react/no-array-index-key
-                  chartData.map((entry, index) => (
-                    <Cell
-                      key={`cell-${index}`}
-                      fill={COLORS[index % COLORS.length]}
-                    />
-                  ))
-                }
-              </Pie>
-            </PieChart>
-          </ResponsiveContainer>
-        </div> */}
-      </Card.Session>
-    </Card>
+        </Card.Session>
+      </Card>
+    </div>
   )
 }

e251abf177926992cb1a807249e61295e62e99b1
Groolt 98 <binhnguyen9803@gmail.com>
2023-12-08 20:25:54 +0700

    API Products HomePage

--- app/(pages)/collection/collection-list.jsx
+++ app/(pages)/collection/collection-list.jsx
@@ -1,8 +1,7 @@
-import CardProduct from '../../../components/card/card-product'
 import CardBanner from '../../../components/card/card-banner'
-import { Button } from '@/components/ui/button'
+import CardProductTest from '../../../components/card/card-product-test'
 
-const CollectionList = () => {
+const CollectionList = ({ data }) => {
   return (
     <div className="max-w-full">
       <div className="grid lg:grid-cols-5 md:grid-cols-3 gap-5 grid-cols-2 pt-[45px] max-w-full px-[30px] mx-auto">
@@ -11,25 +10,10 @@ const CollectionList = () => {
             'https://media.coolmate.me/cdn-cgi/image/quality=80/image/August2023/Rectangle_245_(11).png'
           }
         />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-        <CardProduct />
-      </div>
-      <div className="flex items-center justify-center flex-col py-[20px] relative mt-[40px] gap-5">
-        <Button
-          variant="outline"
-          className="rounded-[1.5rem] bg-black text-white uppercase h-[45px] py-[0.875rem] px-[2.875rem] font-[700] hover:bg-gray-300/60 hover:text-black"
-        >
-          Xem thêm
-        </Button>
-        <p className="opacity-60 text-xs">
-          Hiển thị 1 - 18 trên tổng số 88 sản phẩm
-        </p>
+        {data &&
+          data.map((product, index) => {
+            return <CardProductTest key={index} product={product} />
+          })}
       </div>
     </div>
   )
